6e6898575117f7fcd344b7347b0a00a4e4442c81
Panqueka0 <37937158+Panqueka0@users.noreply.github.com>
2018-03-30 23:12:15 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,10 +3,10 @@ const whitey = new Discord.Client();
 
 whitey.on('guildMemberAdd', member => {
    //member.guild.channels.get('414151682264662016').send(member.user.toString()+' Ei seu bunda mole');
-   member.guild.channels.get('414151682264662016').send(`${member} Ola parceiro, como vai a vida? ('vai bem ou vai mal`);
+   member.guild.channels.get('414151682264662016').send(`${member} Ola parceiro, como vai a vida? ('vai bem ou vai mal');
 });
 
-/*whitey.on('message', message => {
+whitey.on('message', message => {
     if (message.content === 'vai bem') 
     {
         message.reply('Que bom, gostaria de ganhar umas pratas a mais trabalhando para a justiça?? (lSim/lNão)');
@@ -38,6 +38,5 @@ whitey.on('guildMemberAdd', member => {
         
     }
 });
-*/
 // THIS  MUST  BE  THIS  WAY
-serginho.login(process.env.BOT_TOKEN);
+whitey.login(process.env.BOT_TOKEN);
