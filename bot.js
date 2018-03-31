0d6264d935f1f8f72b62d847f1cd23a06dfa6a71
Panqueka0 <37937158+Panqueka0@users.noreply.github.com>
2018-03-30 23:28:05 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,8 +5,7 @@ whitey.on('guildMemberAdd', member => {
    //member.guild.channels.get('414151682264662016').send(member.user.toString()+' Ei seu bunda mole');
    member.guild.channels.get('414151682264662016').send('${member} Ola parceiro, como vai a vida? ('vai bem ou vai mal');
 });
-whitey.on('message', message => 
-{
+whitey.on('message', message => {
     if (message.content === 'vai bem') 
     {
         message.reply('Que bom, gostaria de ganhar umas pratas a mais trabalhando para a justiça?? (lSim/lNão)');
